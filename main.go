418a677ea559ee65d78a91addcf44e76a3f5f61a
Chee Zhong Wei <cheezhongwei00@gmail.com>
2023-02-20 00:22:26 +0800

    Abstract out the controllers

--- main.go
+++ main.go
@@ -1,41 +1,19 @@
 package main
 
 import (
-	"net/http"
+	"admin-handling/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	router := gin.Default()
 
-	router.GET("/api/commonstudents", getCommonStudents)
-	router.POST("/api/register", registerStudents)
-	router.POST("/api/suspend", suspendStudent)
-	router.POST("/api/retrievefornotifications", retrieveNotifications)
+	router.GET("/api/commonstudents", controllers.GetCommonStudents)
+	router.POST("/api/register", controllers.RegisterStudents)
+	router.POST("/api/suspend", controllers.SuspendStudent)
+	router.POST("/api/retrievefornotifications", controllers.RetrieveNotifications)
 
 	router.Run("localhost:8080")
-}
-
-func getCommonStudents(c *gin.Context) {
-	// TODO
-	students := 1 // placeholder
-
-	c.IndentedJSON(http.StatusOK, students)
-}
-
-func registerStudents(c *gin.Context) {
-	// TODO
-	c.Status(http.StatusNoContent)
-}
-
-func suspendStudent(c *gin.Context) {
-	// TODO
-	c.Status(http.StatusNoContent)
-}
-
-func retrieveNotifications(c *gin.Context) {
-	// TODO
-	students := 1
-	c.IndentedJSON(http.StatusOK, students)
 }
