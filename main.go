70fcb4bb28a57eef988fc359abc0bcec4a2c82fb
Chee Zhong Wei <cheezhongwei00@gmail.com>
2023-02-19 16:05:28 +0800

    Add dummy API endpoints

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/api/commonstudents", getCommonStudents)
+	router.POST("/api/register", registerStudents)
+	router.POST("/api/suspend", suspendStudent)
+	router.POST("/api/retrievefornotifications", retrieveNotifications)
+
+	router.Run("localhost:8080")
+}
+
+func getCommonStudents(c *gin.Context) {
+	// TODO
+	students := 1 // placeholder
+
+	c.IndentedJSON(http.StatusOK, students)
+}
+
+func registerStudents(c *gin.Context) {
+	// TODO
+	c.Status(http.StatusNoContent)
+}
+
+func suspendStudent(c *gin.Context) {
+	// TODO
+	c.Status(http.StatusNoContent)
+}
+
+func retrieveNotifications(c *gin.Context) {
+	// TODO
+	students := 1
+	c.IndentedJSON(http.StatusOK, students)
 }
